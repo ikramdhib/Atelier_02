437c308e6eecdce2a093b6c40de2b6c6cdfbf921
ikramdhib <dhibikram50@gmail.com>
2023-02-08 17:24:07 +0100

    Partie -2- p1

--- src/Controller/ClubController.php
+++ src/Controller/ClubController.php
@@ -8,6 +8,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ClubController extends AbstractController
 {
+
+
     #[Route('/club', name: 'app_club')]
     public function index(): Response
     {
@@ -23,5 +25,30 @@ class ClubController extends AbstractController
         'name' => $nom,
     ]); 
  
+    }
+
+    #[Route('/club/list', name:'app_list')]
+    public function list () :Response{
+
+        $formations =  array(
+            array('ref' => 'form147', 'Titre' => 'Formation Symfony
+            4','Description'=>'formation pratique',
+            'date_debut'=>'12/06/2020', 'date_fin'=>'19/06/2020',
+            'nb_participants'=>19) ,
+
+            array('ref'=>'form177','Titre'=>'Formation SOA' ,
+            'Description'=>'formation theorique','date_debut'=>'03/12/2020','date_fin'=>'10/12/2020',
+            'nb_participants'=>0),
+
+            array('ref'=>'form178','Titre'=>'Formation Angular' ,
+            'Description'=>'formation theorique','date_debut'=>'10/06/2020','date_fin'=>'14/06/2020',
+            'nb_participants'=>12)
+
+        );
+
+
+        return $this->render('club/list.html.twig' , [
+            'formations'=>$formations
+        ]);
     }
 }
