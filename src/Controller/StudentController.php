877ba683ae72ee8ee28169f81c7f1c4d4d17826b
ikramdhib <dhibikram50@gmail.com>
2023-02-25 14:50:17 +0100

    Atelier4

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Student;
+use App\Repository\StudentRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class StudentController extends AbstractController
 {
-    #[Route('/student', name: 'app_student')]
-    public function index(): Response
+   /* #[Route('/student', name: 'app_student')]
+    public function index(StudentRepository $repo): Response
     {
+       $students =$repo->findAll();
+
         return $this->render('student/index.html.twig', [
-            'controller_name' => 'StudentController',
+            'students' => $students,
+        ]);
+    }*/
+
+    #[Route('/student', name: 'app_student')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $repo=$doctrine->getRepository(Student::class);
+
+        $students=$repo->findAll();
+
+        return $this->render('student/index.html.twig', [
+            'students' => $students,
         ]);
     }
 
@@ -21,4 +38,40 @@ class StudentController extends AbstractController
     public function index2(): Response{
         return new Response("Bonjour Mes Etudiants");
     } 
+
+
+    #[Route('/deleteStudent/{id}', name:'app_deleteg')]
+    public function deletStudent($id , ManagerRegistry $mngr) {
+
+        $student = $mngr->getRepository(Student::class)->find($id);
+
+        $em=$mngr->getManager();
+
+        $em->remove($student);
+        $em->flush();
+
+        return $this->redirectToRoute('app_student');
+
+    }
+
+
+    #[Route('/addUser', name:'app_deleteg')]
+    public function addStudent(ManagerRegistry $mngr){
+
+
+        $student = new Student();
+        $student->setUsernamr("hhkjhk");
+        $student->setEmail("koktooo@gmail.com");
+
+
+        $em=$mngr->getManager();
+
+        $em->persist($student);
+        $em->flush();
+
+        return $this->redirectToRoute('app_student');
+
+    }
+
+
 }
